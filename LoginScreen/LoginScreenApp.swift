17818a6a5eff73d7788ec3eeda397bfe8fdb766c
DenisMihEpam <denis_mikhaylovskiy@epam.com>
2021-09-26 18:02:39 +0300

    Initial Commit

---
+++ LoginScreen/LoginScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginScreenApp.swift
+//  LoginScreen
+//
+//  Created by denis mikhaylovsky on 26.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct LoginScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
